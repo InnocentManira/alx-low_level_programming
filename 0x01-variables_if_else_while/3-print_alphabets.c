4a1526606467f837472f96448feed28618d33c89
InnocentManira <innocentmaniragena96@gmail.com>
2023-03-20 08:25:38 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main- print if number is positive, zero or negative
+ * Return: Always (Success)
+ *
+ */
+int main(void)
+
+{
+	int low;
+
+	for (low = 'a'; low <= 'z'; low++)
+	putchar(low);
+	for (low = 'a'; low <= 'z'; low++)
+	putchar(low);
+	putchar('\n');
+	return (0);
+}
